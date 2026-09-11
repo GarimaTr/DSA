13c5147874168177f7a548428085cf543f657da8
GarimaTr <garimatripathi.2022@gmail.com>
2026-09-11 07:46:46 +0530

    Day1

--- DSA_basic/Day1.java
+++ DSA_basic/Day1.java
@@ -1,4 +1,9 @@
 public class Day1 {
     public static void main(String[] args) {
-        System.out.println("heyyyyyyyyyy!!!!!!!!!");
+      int n= a.length+b.length;
+      
+      for(int i=0;i<n;i++){
+        
+      }
     }
+}
